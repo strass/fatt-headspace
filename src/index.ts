49e81c5466d62bbd98aadf66ee57d1ff0c08f6a7
Zak Strassberg <zakstrassberg@gmail.com>
2019-07-07 17:35:21 -0400

    dynamic message from github

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
-import Discord, { TextChannel, Message } from 'discord.js';
+import Discord, { TextChannel, Message } from "discord.js";
+import fetch from "node-fetch";
 
-const sleep = async (timeout: number) => new Promise(res => setTimeout(res, timeout));
+const sleep = async (timeout: number) =>
+  new Promise(res => setTimeout(res, timeout));
 
 export default async (event: any) => {
   let done = false;
   let seconds = 0;
   const client = new Discord.Client();
 
-  client.on('ready', async () => {
+  client.on("ready", async () => {
     console.log(`ready`);
+
     try {
       const headspace = client.channels.find(
         (_v, key) => key === process.env.CHANNEL_ID
@@ -16,7 +19,7 @@ export default async (event: any) => {
         // "590682286438088704" // testing channel
       ) as TextChannel;
 
-      if (headspace && headspace.type === 'text') {
+      if (headspace && headspace.type === "text") {
         try {
           headspace.startTyping();
           while ((await headspace.fetchMessages({ limit: 100 })).size > 0) {
@@ -24,59 +27,47 @@ export default async (event: any) => {
             console.log(`deleted ${deleted.size} messages`);
           }
         } catch (ex) {
-          console.error('error clearing messages');
+          console.error("error clearing messages");
         }
 
         let repost: any = false;
         try {
-          console.log('reposting message');
-          repost = (await headspace.send(`This is our bad headspace channel, a place to clear your mind, vent, give and receive support! You can remove yourself from this channel with ".iam avoiding-badheadspace" in #i-am-i-am-not at any time. Use this as needed, the option is here for your health and peace of mind!
-
-Because of the nature of this channel, we have some additional rules:
--Messages in this channel are cleared at 10:30 AM EST for everyone's safety and privacy. Don't screen cap messages here and post them elsewhere, doing so will result in a ban from the server
--Never @ another member in this channel
--Ask before giving advice
--Don't ask anyone for details they aren't willing to share or assume anything about their situation.
--Don't be dismissive of other people's feelings, don't try to one-up or eclipse their problems with your own. Let's all be understanding and uplift each other.
--Be considerate of other people's tolerance for jokes or despair about the topics discussed here. Jokes can be triggering or upsetting in ways you may not intend and giving into hopelessness helps no one.
-
-Please reach out to a mod if you feel someone is being disrespectful or out of line instead of escalating. Access to this channel can be revoked and attempts to bypass that will result in being banned from the server.
-
-Should you need them, here are some resources:
-Lines for Life: <https://www.linesforlife.org/>
-800 273 8255 / Text 273TALK to 839863
-Trans Lifeline: <https://www.translifeline.org/>
-US: 877 565 8860 / CA: 877 330 6366
-Crisis Text Line: <https://www.crisistextline.org/>
-Text HOME to 741741`)) as Message;
-          console.log('!!!');
+          console.log("fetching template");
+          const response = await fetch(
+            "https://raw.githubusercontent.com/strass/fatt-headspace/master/template.txt"
+          );
+          const text = await response.text()
+          console.log("body response:");
+          console.log(text);
+          console.log("reposting message");
+          repost = (await headspace.send(text)) as Message;
+          console.log("!!!");
           console.log(repost);
-          console.log('!!!');
+          console.log("!!!");
         } catch (ex) {
           console.error(`Failed reposing message:`, ex);
         }
-
         try {
           if (repost) {
-            console.log('attempting to pin');
+            console.log("attempting to pin");
             repost.pin();
           }
         } catch (ex) {
           console.error(`Failed pinning message:`, ex);
         } finally {
-          console.log('stop typing');
+          console.log("stop typing");
           headspace.stopTyping();
         }
       } else {
         console.error(
-          'Something went wrong. Expected a text channel but got: ',
+          "Something went wrong. Expected a text channel but got: ",
           JSON.stringify(headspace)
         );
       }
     } catch (ex) {
       console.error(ex);
     } finally {
-      console.log('finally case');
+      console.log("finally case");
       done = true;
     }
   });
@@ -88,7 +79,7 @@ Text HOME to 741741`)) as Message;
     await sleep(1000);
   }
 
-  console.log('done thinking');
+  console.log("done thinking");
 
   return { seconds };
 };
